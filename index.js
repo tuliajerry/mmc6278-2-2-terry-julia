3d1d5cdf10fe2da16ee642bd10993aa7cc954449
tuliajerry <156032724+tuliajerry@users.noreply.github.com>
2024-09-01 18:53:52 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const { program } = require("commander");
+const fs = require("fs/promises");
+const chalk = require("chalk");
+const QUOTE_FILE = "quotes.txt";
+
+program
+  .name("quotes")
+  .description("CLI tool for inspiration")
+  .version("0.1.0");
+
+program
+  .command("getQuote")
+  .description("Retrieves a random quote")
+  .action(async () => {
+    // TODO: Pull a random quote from the quotes.txt file
+    // console log the quote and author
+    // You may style the text with chalk as you wish
+  });
+
+program
+  .command("addQuote <quote> [author]")
+  .description("adds a quote to the quote file")
+  .action(async (quote, author) => {
+    // TODO: Add the quote and author to the quotes.txt file
+    // If no author is provided,
+    // save the author as "Anonymous".
+    // After the quote/author is saved,
+    // alert the user that the quote was added.
+    // You may style the text with chalk as you wish
+    // HINT: You can store both author and quote on the same line using
+    // a separator like pipe | and then using .split() when retrieving
+  });
+
+program.parse();
